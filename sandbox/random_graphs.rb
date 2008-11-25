9ce8579e971e896547045600867409c790a85717
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2008-11-25 00:56:07 +0000

    Criado o modelo de grafo aleatorio de Bollobas. Compute_metrics agora pode receber varios arquivos. Create_report generalizado: agora produz saida em HTML e em CSV.

--- sandbox/random_graphs.rb
+++ sandbox/random_graphs.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'rgl/adjacency'
+require 'rgl/bidirectional'
 require 'igraph'
 # g.each_edge(IGraph::EDGEORDER_ID) { |x, y| puts "depends #{x} #{y}" }
 
@@ -18,12 +19,27 @@ def choose_random(weights)
   return choose_random_acc(acc_weights)
 end
 
+class RGL::DirectedAdjacencyGraph
+  def in_degree(v)
+    i = 0
+    self.each_edge { |a, b| i += 1 if b == v }
+    return i
+  end
+end
+
 # "Directed Scale-Free Graphs", Bollobas et al.
-def bollobas_game(alpha, beta, gamma, delta_in, delta_out, n)
+# Suggested parameters for a web graph (for which gamma_in = 2.1 and
+# gamma_out = 2.7)
+# * alpha =     0.41
+# * beta =      0.49 .. 0.59
+# * gamma =     0.10 .. 0.00
+# * delta_in =  0.00 .. 0.24
+# * delta_out = 0.00
+def bollobas_game(n, alpha, beta, gamma, delta_in, delta_out)
   sum = 0
   event_prob_acc = [alpha, beta, gamma].map { |x| sum += x; sum }
 
-  g = RGL::AdjacencyGraph.n ew
+  g = RGL::DirectedAdjacencyGraph.new
   g.add_edge(0, 0)
   1.upto(n - 1) do |i|
     event = choose_random_acc(event_prob_acc)
@@ -31,29 +47,33 @@ def bollobas_game(alpha, beta, gamma, delta_in, delta_out, n)
     when 0
       # add a new vertex v together with an edge from v to an existing 
       # vertex w, where w is chosen according to d_in + delta_in
-      vertex_prob = g.vertices.map { |v| v.in_degree + delta_in }
+      vertex_prob = g.vertices.map { |x| g.in_degree(x) + delta_in }
       w = choose_random(vertex_prob)
-      g.add_edge(i, g.vertices(w))
+      g.add_vertex(i)
+      g.add_edge(i, g.vertices[w])
     when 1
       # add an edge from an existing vertex v to an existing vertex w, where
       # v and w are chosen independently, v according to d_out + delta_out,
       # and w according to d_in + delta_in
-      v = choose_random(g.vertices.map { |x| x.out_degree + detal_out })
-      w = choose_random(g.vertices.map { |x| x.in_degree + detal_in })
-      g.add_edge(g.vertice(v), g.vertices(w))
+      v = choose_random(g.vertices.map { |x| g.out_degree(x) + delta_out })
+      w = choose_random(g.vertices.map { |x| g.in_degree(x) + delta_in })
+      g.add_edge(g.vertices[v], g.vertices[w])
     when 2
       # add a new vertex w and an edge from an existing v to w, where v is
       # chosen according to d_out + delta_out
-      vertex_prob = g.vertices.map { |v| v.out_degree + delta_out }
+      vertex_prob = g.vertices.map { |x| g.out_degree(x) + delta_out }
       v = choose_random(vertex_prob)
-      g.add_edge(g.vertices(v), i)
+      g.add_vertex(i)
+      g.add_edge(g.vertices[v], i)
     end
   end
+
+  return g
 end
 
 # Barabasi-Albert model. TODO: use parameters m0, m
 #def ba_game(m0, m)
-#  g = RGL::AdjacencyGraph.new
+#  g = RGL::DirectedAdjacencyGraph.new
 #  g.add_edge(0, 1)
 #
 #  2.upto(m - 1) do |node|
