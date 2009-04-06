697977c51a2393c9db3fa0da6fe468cf4b402387
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2009-04-06 21:36:34 +0000

    atualizacao do javex.sh

--- trunk/sandbox/javex.sh
+++ trunk/sandbox/javex.sh
@@ -3,4 +3,9 @@
 # Exemplo de como extrair fatos de design a partir de arquivos .class usando
 # o javex.
 
-find -name '*.class' -exec javex -z -f -l '{}' \; > fatos.ta
+DIR=$1 || '.'
+find $DIR -name '*.class' -exec javex -z -f -l '{}' \; > l0.ta
+grok lift_to_classlevel.grok l0.ta l1.tmp
+grep -v '^contain' l1.tmp > l1.rsf
+rm -f l1.tmp
+
