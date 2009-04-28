38fc7786300e8a93d2ace0c79fd72ffe51015c32
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2009-04-28 23:17:15 +0000

    stat_analysis agora esta funcionando.

--- sandbox/stat_analysis.R
+++ sandbox/stat_analysis.R
@@ -6,16 +6,19 @@ v = read.table("vertices.data", header=T)
 c = read.table("clusters.data", header=T)
 g = read.table("global.data", header=T)
 
+
+
 # Degree distribution
-fit = plfit(v$deg)
-write(fit, file="stat.data", append=T)
+fit = plfit(v$deg[v$deg > 0])
+write(c("degree", fit$xmin, fit$alpha, fit$D), file="stat.data", append=F)
 
 # Community size
-fit = plfit(c$size)
-write(fit, file="stat.data", append=T)
+fit = plfit(c$size[c$size > 0])
+write(c("moduleSize", fit$xmin, fit$alpha, fit$D), file="stat.data", append=T)
 
 # Number of nodes
-write(g$size)
+write(c("nodes", g$size), file="stat.data", append=T)
 
 # Mixing parameter
-write(g$mixing)
+write(c("mixing", g$mixing), file="stat.data", append=T)
+
