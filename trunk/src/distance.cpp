7900af31a004aa0712f49369abc8e1a2c10eca5b
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2009-05-13 19:54:45 +0000

    distance implementado com simulated annealing e com algoritmo genetico. a segunda alternativa eh muito lenta.

--- trunk/src/distance.cpp
+++ trunk/src/distance.cpp
@@ -6,21 +6,36 @@
  * "Measuring distances between complex networks"
  *
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <float.h>
-#include <math.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cfloat>
+#include <cmath>
+#include <cstring>
 
+#include <ga/GASimpleGA.h>
+#include <ga/GA1DArrayGenome.h>
+#include <ga/GAArray.h>
 
-typedef unsigned char dist;
+using namespace std;
+
+// GLOBAL variables and declarations
+typedef unsigned char t_dist;
+typedef GA1DArrayGenome<short> Genome;
+t_dist *g_matrix1, *g_matrix2;
+short *g_indices;
+int g_size;
+
+void GenomeToIndices(Genome &g, short *indices, int n);
+
 
 
 void help() {
   puts("Arguments: N distance-matrix1 distance-matrix2");
+  exit(1);
 }
 
-float distance(dist *a, dist *b, int n) {
+
+float distance(t_dist *a, t_dist *b, int n) {
   float dif, sum;
   int i, j;
 
@@ -38,7 +53,7 @@ float distance(dist *a, dist *b, int n) {
 /**
  * b is reindexed by indices
  */
-float distance_indices(dist *a, dist *b, int n, int *indices) {
+float distance_indices(t_dist *a, t_dist *b, int n, int *indices) {
   float dif, sum;
   int i, j, x, y;
 
@@ -54,31 +69,9 @@ float distance_indices(dist *a, dist *b, int n, int *indices) {
   return sum;
 }
 
-float Objective(GAGenome &genome) {
-  MyGenome &indices = (MyGenome &)genome;
-  float dif, sum;
-  int i, j, x, y;
-
-  sum = 0.0;
-  for (i = 0; i < g_size; i++) {
-    x = indices[i];
-    for (j = 0; j < g_size; j++) {
-      y = indices[j];
-      dif = b[x*n+y] - a[x*n+y];
-      sum += dif * dif;
-    }
-  }
-  return sum;
-}
-
-float Initializer(GAGenome &g) {
-  g.resize(g_size);
-  for (int i = 0; i < g_size; i++)
-    g.gene(i, rand() % (g_size - i));
-}
 
 /* Takes a nXn matrix, a, and swaps two indices, i, j < n */
-inline float swap_indices(dist *b, int n, int i, int j) {
+inline float swap_indices(t_dist *b, int n, int i, int j) {
   int l, tmp;
   /* swap i, j */
   for (l = 0; l < n; l++) {
@@ -94,7 +87,7 @@ inline float swap_indices(dist *b, int n, int i, int j) {
 }
 
 /* Sorts a matrix by the given indices */
-inline void sort_matrix(dist *b, int n, int *indices) {
+inline void sort_matrix(t_dist *b, int n, int *indices) {
   int i;
 
   for (i = 0; i < n; i++) {
@@ -103,10 +96,10 @@ inline void sort_matrix(dist *b, int n, int *indices) {
   }
 }
 
-inline void scramble(dist *b, int n, int times) {
+inline void scramble(t_dist *b, int n, int times) {
   int i, j, k;
   int indices[n];
-  dist disttmp;
+  t_dist disttmp;
 
   for (i = 0; i < n; i++)
     indices[i] = i;
@@ -122,7 +115,6 @@ inline void scramble(dist *b, int n, int times) {
   sort_matrix(b, n, indices);
 }
 
-// TODO: implement correctly
 inline void genome_to_indices(int *genome, int *indices, int n) {
   int i, j, k, rank;
   int visited[n];
@@ -143,27 +135,72 @@ inline void genome_to_indices(int *genome, int *indices, int n) {
   }
 }
 
-int main(int argc, char *argv[]) {
-  int genome[10];
-  int indices[10];
-  int i;
+///////////////////////////////////////////////////////////////////////////
 
-  for (i = 0; i < 10; i++)
-    genome[i] = 0;
+void GenomeToIndices(Genome &g, short *indices, int n) {
+  int i, j, k, rank;
+  int visited[n];
+  memset(visited, 0, sizeof(int) * n);
 
-  genome_to_indices(genome, indices, 10);
-
-  for (i = 0; i < 10; i++)
-    printf("%d ", indices[i]);
-  printf("\n");
-  exit(0);
+  for (i = 0; i < n; i++) {
+    rank = g.gene(i);
+    j = 0;
+    k = 0;
+    for (j = 0; ; j = (j + 1) % n) {
+      if (k != rank && !visited[j])
+        k++;
+      else if (k == rank && !visited[j])
+        break;
+    }
+    visited[j] = 1;
+    indices[i] = j;
+  }
 }
 
-// GLOBAL
-dist *g_matrix1, *g_matrix2;
-int g_size;
+#ifdef GALIB
+void Initializer(GAGenome &genome) {
+  Genome &g = (Genome &)genome;
+  g.resize(g_size);
+  for (int i = 0; i < g_size; i++)
+    g.gene(i, rand() % (g_size - i));
+}
 
-#ifdef QWEQWE
+int Mutator(GAGenome &genome, float prob) {
+  Genome &g = (Genome &)genome;
+  if (prob <= 0.0)
+    return 0;
+
+  int nmut = 0;
+  int n = g.length();
+  for (int i = 0; i < n; i++) {
+    if (GAFlipCoin(prob)) {
+      g.gene(i, rand() % (n - i));
+      nmut++;
+    }
+  }
+    
+  return nmut;
+}
+
+float Objective(GAGenome &genome) {
+  Genome &g = (Genome &)genome;
+  float dif, sum;
+  int i, j, x, y;
+
+  GenomeToIndices(g, g_indices, g_size);
+
+  sum = 0.0;
+  for (i = 0; i < g_size; i++) {
+    x = g_indices[i];
+    for (j = 0; j < g_size; j++) {
+      y = g_indices[j];
+      dif = g_matrix2[x*g_size+y] - g_matrix1[x*g_size+y];
+      sum += dif * dif;
+    }
+  }
+  return sum;
+}
+
 int main(int argc, char *argv[]) {
   int n;
 
@@ -173,7 +210,71 @@ int main(int argc, char *argv[]) {
   srand(time(NULL));
   n = atoi(argv[1]);
 
-  dist a[n][n], b[n][n];
+  t_dist a[n][n], b[n][n];
+  short indices[n];
+  g_indices = indices;
+  g_matrix1 = &a[0][0];
+  g_matrix2 = &b[0][0];
+  g_size = n;
+  FILE *file1, *file2;
+  int i, j, k, l, tmp;
+
+  file1 = fopen(argv[2], "r");
+  file2 = fopen(argv[3], "r");
+
+  printf("n = %d\n", n);
+
+  puts("loading files...");
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      fscanf(file1, "%d", &tmp);
+      a[i][j] = tmp;
+      fscanf(file2, "%d", &tmp);
+      b[i][j] = tmp;
+    }
+  }
+  puts("done");
+
+  ///////////////////////////////////////////////////////////////////
+
+  Genome genome(g_size, Objective);
+  genome.initializer(Initializer);
+  genome.mutator(Mutator);
+  GASimpleGA ga(genome);
+
+  ga.nGenerations(32767);
+  ga.minimize();
+  ga.populationSize(8);
+  ga.pCrossover(0.1);
+  ga.pMutation(2.0 / g_size);
+
+  
+  cout << "population size: " << ga.populationSize() << endl;
+  cout << "nGenerations: " << ga.nGenerations() << endl;
+  cout << "mutation p: " << ga.pMutation() << endl;
+  cout << "crossover p: " << ga.pCrossover() << endl;
+
+  ga.initialize();
+  for (int i = 0; i < ga.nGenerations(); i++) {
+    ga.step();
+    cout << i << " " << ga.statistics().minEver() << endl;
+    cout << "   " << ga.statistics().mutations() << endl;
+  }
+
+}
+
+#else
+
+int main(int argc, char *argv[]) {
+  int n;
+
+  if (argc < 4)
+    help();
+
+  srand(time(NULL));
+  n = atoi(argv[1]);
+
+  t_dist a[n][n], b[n][n];
   g_matrix1 = &a[0][0];
   g_matrix2 = &b[0][0];
   g_size = n;
@@ -244,59 +345,3 @@ int main(int argc, char *argv[]) {
   fclose(file2);
 }
 #endif
-
-
-class Permutation {
-private:
-  int *genome;
-  int *indices;
-  dist *a, *b;
-  float distance;
-  int n;
-
-public:
-
-  /* defined permutation */
-  Permutation(int *_genome, int _n, dist *_a, dist *_b) {
-    n = _n;
-    distance = -1;
-    genome = (int *)malloc(n * sizeof(int));
-    memcpy(genome, _genome, n * sizeof(int));
-    indices = (int *)malloc(n * sizeof(int));
-    ::genome_to_indices(genome, indices, n);
-    a = _a;
-    b = _b;
-  }
-  
-  /* random permutation */
-  Permutation(int _n, dist *_a, dist *_b) {
-    int i;
-
-    n = _n;
-    distance = -1;
-    genome = (int *)malloc(n * sizeof(int));
-    for (i = 0; i < n; i++)
-      genome[i] = rand() % (n - i);
-
-    indices = (int *)malloc(n * sizeof(int));
-    ::genome_to_indices(genome, indices, n);
-    a = _a;
-    b = _b;
-  }
-
-  float fitness() {
-    if (distance == -1)
-      distance = ::distance_indices(a, b, n, indices);
-    return distance;
-  }
-};
-/*
-class Permutation : public GAGenome {
-public:
-  GADefineIdentity("Permutation", 201);
-
-  static void init(GAGenome&);
-  static int Mutate(GaGenome&, float);
-  static float Compare(const G
-}
-*/
