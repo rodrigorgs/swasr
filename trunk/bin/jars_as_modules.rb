dbeab64385255ae7c18c20cdae456826d0d9c1af
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2009-05-29 00:50:42 +0000

    adaptacao no tratamento de arquivos jar para facilitar a extracao de arquivos jar dispostos em uma hierarquia de diretorios.

--- trunk/bin/jars_as_modules.rb
+++ trunk/bin/jars_as_modules.rb
@@ -10,6 +10,7 @@ def extract_modules(jar_list)
   pairs = []
   jar_list.each do |zip|
     package = File.basename(zip)
+    puts zip
     Zip::ZipFile.foreach(zip) do |entry|
       if entry.file? && entry.to_s[-6..-1] == '.class'
         klass = entry.to_s.gsub(/\//, '.').sub(/\.class/, '')
@@ -24,10 +25,15 @@ if __FILE__ == $0
   require 'grok'
 
   if ARGV.size < 2
+    puts "
+Usage: #{File.basename($0)} output-file jar-file+
 
+Creates a MODULES file for the JAR files supplied.
+
+"
     exit 1
   end
 
-  pairs = extract_modules(ARGV[0..-2]) #do |zip|
-  puts_pairs(pairs, ARGV[-1])
+  pairs = extract_modules(ARGV[1..-1]) #do |zip|
+  puts_pairs(pairs, ARGV[0])
 end
