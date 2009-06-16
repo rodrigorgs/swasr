c0758ba5bd52352c4c3951865f0b0c91a9981fb3
rodrigorgs <rodrigorgs@c0e3efde-b511-11dd-bcea-ed161227d7e4>
2009-06-16 01:30:14 +0000

    correcao em to_mfinder.

--- trunk/sandbox/to_mfinder.rb
+++ trunk/sandbox/to_mfinder.rb
@@ -10,5 +10,8 @@ pairs = read_pairs(input)
 pairs.map! { |p| p.map { |x| x.to_i } }
 max = entities(pairs).max + 1
 pairs.map! { |p| p.map { |x| x == 0 ? max : x } }
+pairs = pairs.select { |a, b| a != b }
 
-puts_pairs(pairs, output)
+File.open(output, 'w') do |f|
+  pairs.each { |p| f.puts "#{p[0]} #{p[1]} 1" }
+end
